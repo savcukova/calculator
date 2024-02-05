4da16dfed1bf3602f5c100ab15f0473583ae5f8e
savcukova <olinkasavcuk@gmail.com>
2024-02-05 16:49:58 +0100

    show selection

--- calculator.py
+++ calculator.py
@@ -1,32 +1,30 @@
-
-
-
-def calculator():
-    """
-    Main function of the program!
-    """
-    selection = ["+", "-", "*", "/", "pow", "avg", "quit"]
+def calculator() -> None:
+    selection = ('+', '-', '*', '/', 'pow', 'avg', 'quit')
+    
     while True:
         show_selection(selection)
         break
-    
-    
-def show_selection(*args: list) -> None:
+        
+        
+def show_selection(*args) -> None:
     """
     Description:
-    ------------
     Connects values from *args with .join method.
-    Then adds separator before and after parameters.
+    Then adds separator before and after arguments.
     
-    Example:
-    --------
-    args = ("a", "b", "c")
-    
+    Example: 
+    args = ("a", "b", "C")
+
     Result:
-    -------
-    a | b | c
-    ----------
-    """
-    joined = " | ".join(*args)
-    sepatator = "-" * len(joined)
-    print(sepatator, joined, sepatator, sep="/n")
+    ---------
+    a | b | C
+    ---------
+    """    
+    joined = ' | '.join(*args)
+    separator = '-' * len(joined)
+    print(separator, joined, separator, sep="\n")
+    
+    
+    
+if __name__ == "__main__":
+    calculator()
