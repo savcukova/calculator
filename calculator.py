3f88ab42569ac6b2fa111029eb1c06fd1e9f2333
savcukova <olinkasavcuk@gmail.com>
2024-02-05 19:45:38 +0100

    average

--- calculator.py
+++ calculator.py
@@ -1,10 +1,22 @@
+import os
+
+
 def calculator() -> None:
     selection = ('+', '-', '*', '/', 'pow', 'avg', 'quit')
     
     while True:
         show_selection(selection)
         
-        choice = input("")
+        choice = input("Select matmematical operation: ")
+        os.system("clear")
+        
+        if choice == "quit":
+            print("Goodbye")
+            break
+        elif choice == "pow":
+            power()
+        elif choice == "avg":
+            average()
         
         
 def show_selection(*args) -> None:
@@ -40,9 +52,44 @@ def power() -> None:
     Result:
     25
     """
-    base = float(input("Base: "))
-    exp = float(input("Exponent: "))
+    while True:
+        base_input = (input("Base: "))
+        exp_input = (input("Exponent: "))
+        
+        if base_input.isdigit() and exp_input.isdigit():
+            base = float(base_input)
+            exp = float(exp_input)
+            break
+        else:
+            print("Enter a digit!")
     print(base ** exp)
+        
+
+def average() -> None:
+    """
+    Description:
+    Takes values from input and stores them to list.
+    Values must be numeric.
+    If input '=', func calculates average as sum/lengh of list.
+        
+    Example:
+    numbers = [1, 2, 3, 4, 5]
+
+    Result:
+    Average = 3
+    """
+    numbers = []
     
+    while (value := input("Number: ")) != "=":
+        if value.isnumeric():
+            numbers.append(int((value)))
+    result = sum(numbers) / len(numbers)
+    print(result)
+
+
+def arithmetic_operations() -> None:
+        
+        
+        
 if __name__ == "__main__":
     calculator()
